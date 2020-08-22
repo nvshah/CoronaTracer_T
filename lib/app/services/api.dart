1d96df67a9b10654dcab67385b46d0a89a30984e
nvshah <nipunshah6776@gmail.com>
2020-08-22 23:41:10 +0530

    endpoint uri api service for endpoint req/res

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -3,6 +3,15 @@ import 'package:flutter/foundation.dart';
 import './api_keys.dart';
 
 //List all endpoints
+enum Endpoint{
+  cases,
+  casesSuspected,
+  casesConfirmed,
+  deaths,
+  recovered,
+}
+
+//Provide apikey & apiurl for endpoint
 class API {
   final String apiKey;
   static final String host = 'ncov2019-admin.firebaseapp.com';
@@ -16,4 +25,20 @@ class API {
         host: host,
         path: 'token',
       );
+
+  //prepare Uri to fire endpoint api particular functionality
+  Uri endpointUri(Endpoint endPoint) => Uri(
+    scheme: 'https',
+    host: host,
+    path: _paths[endPoint],
+  ); 
+  
+  //Map for endpoints
+  static Map<Endpoint, String> _paths = {
+    Endpoint.cases: 'cases',
+    Endpoint.casesConfirmed: 'casesConfirmed',
+    Endpoint.casesSuspected: 'caseSuspected',
+    Endpoint.deaths: 'deaths',
+    Endpoint.recovered: 'recovered',
+  };
 }
--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:coronatracker/app/services/api.dart';
@@ -7,6 +10,7 @@ class APIService {
 
   APIService(this.api);
 
+  //TOKEN
   Future<String> getAccessToken() async {
     final response = await http.post(
       api.tokenUri().toString(),
@@ -14,5 +18,52 @@ class APIService {
         'Authorization': 'Basic ${api.apiKey}',
       },
     );
+
+    //Successful
+    if(response.statusCode == 200){
+      final data = json.decode(response.body);
+      final accessToken = data['access_token'];
+      if(accessToken != null){
+        return accessToken;
+      }
+    }
+    //Something went Wrong
+    print('Request ${api.tokenUri()} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
+    //Will be handle at Presentation(UI) layer
+    throw response;
   }
+
+  //PAYLOAD
+  Future<int> getEndpointData(@required String accesstToken, @required Endpoint endPoint) async{
+    final uri = api.endpointUri(endPoint);
+    final response = await http.post(uri.toString(), headers: {
+      'Authorization': 'Bearer ${api.apiKey}',
+    });
+
+    if(response.statusCode == 200){
+      final List<dynamic> data = json.decode(response.body);
+      if(data.isNotEmpty){
+        final Map<String, dynamic> endPointData = data[0];
+        final String responseJsonInfoKey = _responseJsonKeysForInfo[endPoint];
+        final int information = endPointData[responseJsonInfoKey];
+        if(information != null){
+          return information;
+        }
+      }
+    }
+    //Something went Wrong
+    print('Request ${api.tokenUri()} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
+    //Will be handle at Presentation(UI) layer
+    throw response;
+  }
+  
+  //Map to get key name for information from payload data
+  static Map<Endpoint, String> _responseJsonKeysForInfo = {
+    Endpoint.cases: 'cases',
+    Endpoint.casesConfirmed: 'data',
+    Endpoint.casesConfirmed: 'data',
+    Endpoint.casesSuspected: 'data',
+    Endpoint.deaths: 'data',
+    Endpoint.recovered: 'data',
+  };
 }
